c7eb1f2deb40001feb4531cd8a657d5670011db7
Nicolas Walter <nicolaswalter2003@gmail.com>
2026-06-24 17:52:53 +0200

    fix(terraform): update .gitignore and add Terraform configurations for AKS and MySQL

--- azure/main.tf
+++ azure/main.tf
@@ -21,3 +21,141 @@ resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
 }
+
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.resource_group_prefix}-aks-${random_string.name.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.resource_group_prefix}-aks-${random_string.name.result}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2ms"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+  }
+
+  tags = {
+    Environment = "Test"
+  }
+}
+
+# Log Analytics Workspace for Monitoring
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${var.resource_group_prefix}-law-${random_string.name.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# Link AKS to ACR for drawing images (AcrPull)
+resource "azurerm_role_assignment" "aks_acr" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
+# Dynamic passwords for MySQL and JWT
+resource "random_password" "mysql_admin_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "random_password" "jwt_secret" {
+  length  = 32
+  special = false
+}
+
+# Key Vault configuration
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "${var.resource_group_prefix}-kv-${random_string.name.result}"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set"
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "mysql_password" {
+  name         = "mysql-admin-password"
+  value        = random_password.mysql_admin_password.result
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "jwt_secret" {
+  name         = "jwt-secret"
+  value        = random_password.jwt_secret.result
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+# Managed MySQL Flexible Server
+resource "azurerm_mysql_flexible_server" "mysql" {
+  name                         = "${var.resource_group_prefix}-mysql-${random_string.name.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  administrator_login          = "dbadmin"
+  administrator_password       = random_password.mysql_admin_password.result
+  backup_retention_days        = 7
+  sku_name                     = "B_Standard_B1ms"
+  version                      = "8.0.21"
+}
+
+# Create "todos" and "auth" databases
+resource "azurerm_mysql_flexible_database" "todos" {
+  name                = "todos"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  charset             = "utf8mb4"
+  collation           = "utf8mb4_unicode_ci"
+}
+
+resource "azurerm_mysql_flexible_database" "auth" {
+  name                = "auth"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  charset             = "utf8mb4"
+  collation           = "utf8mb4_unicode_ci"
+}
+
+# Allow other Azure services to connect to MySQL (e.g. AKS pods)
+resource "azurerm_mysql_flexible_server_firewall_rule" "allow_azure_services" {
+  name                = "allow-azure-internal"
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  resource_group_name = azurerm_resource_group.rg.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+# Disable secure transport enforcement to match the development knex config
+resource "azurerm_mysql_flexible_server_configuration" "disable_ssl" {
+  name                = "require_secure_transport"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  value               = "OFF"
+}
+
--- azure/variables.tf
+++ azure/variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_location" {
   type        = string
-  default     = "francecentral"
+  default     = "swedencentral"
   description = "resources location"
 }
 
